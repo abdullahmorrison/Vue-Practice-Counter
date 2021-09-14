707dbc95878bafefba5d2d4db932d95702ecdc37
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-09-14 09:44:02 -0400

    created counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{num}}</h1>
+  <button @click="onClick()">Increment</button>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return{
+      num: 0
+    }
+  },
+  methods:{
+    onClick(){
+      this.num++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
